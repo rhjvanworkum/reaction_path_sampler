1be0c7b07b5dae3c0f606cd1fcd328bd0f569e97
Ruard van Workum <rhjvanworkum@gmail.com>
2023-05-12 10:39:07 +0200

    yarp test

--- test.py
+++ test.py
@@ -1,80 +1,162 @@
-# from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-# import autode as ade
-# from autode.species import Complex
-# from autode.bond_rearrangement import get_bond_rearrangs
-# from autode.mol_graphs import reac_graph_to_prod_graph
-
-# from tqdm import tqdm
-# import time
-
-# import networkx as nx
-# import matplotlib.pyplot as plt
-
-# from XTB import xtb_driver
-
-# def get_opt_energy(args):
-#     conf, complex, method, settings, cores = args
-#     return xtb_driver(
-#         conf,
-#         complex.charge,
-#         complex.mult,
-#         "opt",
-#         method=method,
-#         xcontrol_settings=settings,
-#         n_cores=cores
-#     )
-
-# def conf_to_xyz_string(conf) -> str:
-#     str = f"{len(conf.atoms)}\n \n"
-#     for atom in conf.atoms:
-#         str += f"{atom.atomic_symbol} {round(atom.coord.x, 4)} {round(atom.coord.y, 4)} {round(atom.coord.z, 4)}\n"
-#     return str
-
-
-# if __name__ == "__main__":
-#     # reac_smiles = ["C1=CC=CO1", "C=C"]
-#     # prod_smiles = ["C1=CC(O2)CCC12"]
-#     reactant_smiles = ["C1=C(C(=O)O)C(Cl)=CO1", "C=CCNO"]
-#     product_smiles = ["C(Cl)1=C(C(=O)O)C(O2)CC(CNO)C12"]
-#     start = Complex(*[ade.Molecule(smiles=smi) for smi in reactant_smiles])
-#     start._generate_conformers()
-
-
-#     N_PROCESSES = 8
-#     n_cores = 2
-#     method = "2"
-
-#     arguments = [(conf_to_xyz_string(conf), start, method, None, n_cores) for conf in start.conformers[:8]]
-
-#     t = time.time()
-#     with ThreadPoolExecutor(max_workers=N_PROCESSES) as executor:
-#         results = list(tqdm(executor.map(get_opt_energy, arguments), total=len(arguments)))
-#     print(time.time() - t)
-
-
+from typing import List
+import os
 import numpy as np
 
-a = np.array([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [1, 0, 0]
-])
+def xyz_parse(input,read_types=False):
 
-b = 2 * a
+    # Commands for reading only the coordinates and the elements
+    if read_types is False:
+        
+        # Iterate over the remainder of contents and read the
+        # geometry and elements into variable. Note that the
+        # first two lines are considered a header
+        with open(input,'r') as f:
+            for lc,lines in enumerate(f):
+                fields=lines.split()
 
-out = (b[np.newaxis, :, :] - a[:, np.newaxis, :])**2
-out = np.sqrt(np.mean(out, axis=-1))
+                # Parse header
+                if lc == 0:
+                    if len(fields) < 1:
+                        print("ERROR in xyz_parse: {} is missing atom number information".format(input))
+                        quit()
+                    else:
+                        N_atoms = int(fields[0])
+                        Elements = ["X"]*N_atoms
+                        Geometry = np.zeros([N_atoms,3])
+                        count = 0
 
-print(np.argwhere(out == np.min(out)))
-print(out[np.argwhere(out == np.min(out))[0]])
-# Out[21]: array([[2, 1]])
+                # Parse body
+                if lc > 1:
 
-print(np.argmin(out, axis=-1))
-print(np.argmin(out, axis=-2))
+                    # Skip empty lines
+                    if len(fields) == 0:
+                        continue            
 
-print(out)
+                    # Write geometry containing lines to variable
+                    if len(fields) > 3:
 
-# print(out[0, 0])
-# print(out[1, 1])
-# print(out[3, 1])
+                        # Consistency check
+                        if count == N_atoms:
+                            print("ERROR in xyz_parse: {} has more coordinates than indicated by the header.".format(input))
+                            quit()
+
+                        # Parse commands
+                        else:
+                            Elements[count]=fields[0]
+                            Geometry[count,:]=np.array([float(fields[1]),float(fields[2]),float(fields[3])])
+                            count = count + 1
+
+        # Consistency check
+        if count != len(Elements):
+            print("ERROR in xyz_parse: {} has less coordinates than indicated by the header.".format(input))
+
+        return Elements,Geometry
+
+    # Commands for reading the atomtypes from the fourth column
+    if read_types is True:
+
+        # Iterate over the remainder of contents and read the
+        # geometry and elements into variable. Note that the
+        # first two lines are considered a header
+        with open(input,'r') as f:
+            for lc,lines in enumerate(f):
+                fields=lines.split()
+
+                # Parse header
+                if lc == 0:
+                    if len(fields) < 1:
+                        print("ERROR in xyz_parse: {} is missing atom number information".format(input))
+                        quit()
+                    else:
+                        N_atoms = int(fields[0])
+                        Elements = ["X"]*N_atoms
+                        Geometry = np.zeros([N_atoms,3])
+                        Atom_types = [None]*N_atoms
+                        count = 0
+
+                # Parse body
+                if lc > 1:
+
+                    # Skip empty lines
+                    if len(fields) == 0:
+                        continue            
+
+                    # Write geometry containing lines to variable
+                    if len(fields) > 3:
+
+                        # Consistency check
+                        if count == N_atoms:
+                            print("ERROR in xyz_parse: {} has more coordinates than indicated by the header.".format(input))
+                            quit()
+
+                        # Parse commands
+                        else:
+                            Elements[count] = fields[0]
+                            Geometry[count,:] = np.array([float(fields[1]),float(fields[2]),float(fields[3])])
+                            if len(fields) > 4:
+                                Atom_types[count] = fields[4]
+                            count = count + 1
+
+        # Consistency check
+        if count != len(Elements):
+            print("ERROR in xyz_parse: {} has less coordinates than indicated by the header.".format(input))
+
+        return Elements,Geometry,Atom_types
+
+def ob_geo_opt(
+    E,
+    G,
+    adj_mat,
+    ff_name: str = 'UFF', 
+    fixed_atoms: List[int] = [], 
+    n_steps: int = 500
+) -> None:
+    import openbabel as ob
+
+    # load in openbabel modules
+    conv = ob.OBConversion()
+    conv.SetInAndOutFormats('mol','xyz')
+    mol = ob.OBMol()
+
+    # create a mol file for geo_opt
+    opt_file = 'obff.mol'
+    # mol_write(opt_file,E,G,adj_mat,q=q,append_opt=False)
+    
+    conv.ReadFile(mol,opt_file)
+    
+    # Define constraints  
+    constraints= ob.OBFFConstraints()
+    if len(fixed_atoms) > 0:
+        for atom in fixed_atoms: 
+            constraints.AddAtomConstraint(int(atom)) 
+            
+    # Setup the force field with the constraints 
+    forcefield = ob.OBForceField.FindForceField(ff_name)
+    forcefield.Setup(mol, constraints)     
+    forcefield.SetConstraints(constraints) 
+    
+    # Do a conjugate gradient minimiazation
+    forcefield.ConjugateGradients(n_steps)
+    forcefield.GetCoordinates(mol) 
+    
+    # write into xyz file
+    conv.WriteFile(mol,'result.xyz')
+    
+    # parse output xyz file
+    element, geo = xyz_parse("result.xyz")
+
+    # remove mol and xyz file
+    try:
+        os.remove(opt_file)
+        os.remove('result.xyz')
+    except:
+        pass
+
+    return geo
+
+
+
+if __name__ == "__main__":
+    # 1. get product conformer
+
+    # so you wanna take the product elements & geometry but use the adjacency_matrix of the reactants
